381a6ae967b5453f022b77f5830ee2587f75853f
humingxian <hudadacool@163.com>
2020-01-16 14:50:36 +0800

    新增testfunction

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import _ from 'lodash'
 import aDemo1 from 'hudada-demo1/src/index.js'
+import test from './test'
 let arr = ['monorepo', 'lerna', 'yarn', 'workspace', 'git', 'npm']
 arr = _.concat(arr, aDemo1)
 const str = _.join(arr, '~')
+test()
 
 export default str
---
+++ src/test.js
@@ -0,0 +1,4 @@
+export default function test () {
+  console.log('我是testfunction')
+  return 'test'
+}
